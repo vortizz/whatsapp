08cf4903e2669d53ab71b8c69e4104d6b73901b5
Victor Ortiz <ortizvictor154@gmail.com>
2026-05-05 17:15:58 +1000

    added networkidle to register

--- fe/test/e2e/auth.spec.ts
+++ fe/test/e2e/auth.spec.ts
@@ -9,6 +9,7 @@ async function fillRegistrationStep1(
   await page.getByLabel('Password').first().fill(user.password)
   await page.getByLabel('Confirm Password').fill(user.password)
   await page.getByRole('button', { name: 'Next' }).click()
+  await page.waitForLoadState('networkidle')
 }
 
 async function fillPassphrase(page: Page, passphrase: string) {
